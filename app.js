d670a6e64071b1ca74197a45a2bc262389213701
Dashka6248 <167606371+Dashka6248@users.noreply.github.com>
2024-04-20 15:32:39 +0800

    game

---
+++ app.js
@@ -0,0 +1,135 @@
+// switch -> nuhtsul shalgah
+/*var word ="hello"
+switch(word){
+	case "hello":
+		console.log("sain baina uu")
+		break;
+	case "bonjour":
+		console.log("sain baina uu")
+		break;	
+	case "nihao":
+		console.log("sain baina uu")
+		break;
+	default:
+		console.log("iim ug baihgui baina")
+}
+var day="4"
+switch(day){
+	case "1":
+		console.log("Davaa garig")
+		break;
+	case "2":
+		console.log("Myagmar garig")
+		break;	
+	case "3":
+		console.log("Lhagva garig")
+		break;
+	case "4":
+		console.log("Purev garig")
+		break;
+	case "5":
+		console.log("Baasan garig")
+		break;	
+	case "6":
+		console.log("Hagas sain udur")
+		break;
+	case "7":
+		console.log("Buten sain udur")
+		break;
+	default:
+		console.log("Uuchlaarai iim udur baihgui baina")
+}
+var jims="alim"
+switch(jims){
+	case "alim":
+		console.log("ulaan ongotei")
+		break;
+	case "banana":
+		console.log("shar ongotei")
+		break;	
+	case "lemon":
+		console.log("shar ongotei")
+		break;
+	case "guzeelzgene":
+		console.log("ulaan ongotei")
+		break;
+	case "tarvas":
+		console.log("nogoon ongotei")
+		break;
+	default:
+		console.log("Uuchlarai iim jims alga baina")
+}var sar="18"
+switch(sar){
+	case "12":
+	case "1":
+	case "2":
+		console.log("Ovol")
+		break;
+	case "3":
+	case "4":
+	case "5":
+		console.log("Havar")
+		break;	
+	case "6":
+	case "7":
+	case "8":
+		console.log("Zun")
+		break;
+	case "9":
+	case "10":
+	case "11":
+		console.log("Namar")
+		break;
+	default:
+		console.log("Uuchlaarai iim sar baihgui")
+}
+var too1= +prompt("toog oruulna uu")
+var temdegt= prompt("/,*,+,- ali negiig oruulna uu")
+var too2= +prompt("2 dahi toog otuulna uu")
+switch(temdegt){
+	case "*":
+		alert(too1*too2);
+		break;
+	case "/":
+		alert(too1/too2);
+		break;
+	case "+":
+		alert(too1+too2);
+		break;
+	case "-":
+		alert(too1-too2);
+		break;
+}*/
+alert("Sain baina uu Bait Buu togloomd tavtai moril.")
+alert("Tand 3 zevseg baina");
+alert
+alert("1-t Gar buu 2-t Tom buu 3-t Hutga");
+alert("Herev ta alj chadval hojno alj chadahgui bol hojigdono")
+var songolt = prompt("Gar buu/Tom buu/Hutga");
+if(songolt=="Gar buu"){
+	alert("Gar buu dundaj hemjeenii hohirol uchruulna.");
+	var songolt2=prompt("Hun alah/Amitan alah");
+	if(songolt2=="Hun alah"){
+		alert("Ta haraahan alj chadaagui Ta hojigdloo")
+	}else if(songolt2=="Amitan alah"){
+	alert("Ta alsan Ta hojloo")
+	}
+}
+if(songolt=="Tom buu"){
+	alert("Tom buu buten hohirol uchruulna");
+	var songolt2=prompt("Hun alah/Amitan alah");
+	if(songolt2=="Hun alah"){
+		alert("Ta alsan Ta hojloo")
+	}else if(songolt2=="Amitan alah"){
+	alert("Ta alsan Ta hojloo")
+	}
+}
+if(songolt=="Hutga"){
+	alert("Hutga baga hemjeenii hohirol uchruulna.");
+	var songolt2=prompt("Hun alah/Amitan alah");
+	if(songolt2=="Hun alah"){
+		alert("Ta haraahan alj chadaagui Ta hojigdloo")
+	}else if(songolt2=="Amitan alah"){
+	alert("Ta haraahan alj chaadgui Ta hojigdloo")
+	}
+}
